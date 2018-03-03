0b4a6e1d0abfcacdffe8b39454a9612ed36fab3d
almudza <alifulmudzakir@gmail.com>
2018-03-03 20:44:09 +0700

    Relationship Created

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,9 +2,14 @@
 
 namespace App\Model;
 
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    //relationship Review
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
